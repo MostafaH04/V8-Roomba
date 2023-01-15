07cf9547b720f550af8d7b5dd244c3a1a178456a
Siddharth <siddharth.n.viswanath@gmail.com>
2023-01-15 01:19:43 -0500

    Wrote test cases for roomba & dock using RF

--- RF_Comms(dock - test)/RF_Comms(dock - test).ino
+++ RF_Comms(dock - test)/RF_Comms(dock - test).ino
@@ -1,15 +1,34 @@
 #define MOTOR_ROWS 1
-
 #include "Onboard_Comms.hpp"
+#include "RF_Comms.hpp"
+
+//Transmitter - Transmits data from the dock to the roomba
+dataIn_t test_out;
+test_out.dock_state = 1;
+test_out.cv_x_pos = 1.0;
+test_out.cv_y_pos = 2.0;
+test_out.cv_z_pos = 3.0;
+test_out.dock_rx_address = "00001";
+
+Rf_Comms test_comms_out(7, 8, true);
+CommsOut comms;
 
 //Master
-CommsOut comms;
-void setup()
-{
+void setup() {
     Serial.begin(9600);
     comms = CommsOut(SLAVE_ADDR);
+
+    test_comms_out.initAddress();
+    test_comms_out.init();
+
+    test_comms_out.Dock_sendData(test_out);
+    Serial.println("Data sent from dock");
+    delay(1000);
 }
 
 void loop()
 {
+    test_comms_out.Dock_readData(test_in);
+    Serial.println("Data received from roomba");
+    delay(1000);
 }
--- RF_Comms(roomba - test)/RF_Comms(roomba - test).ino
+++ RF_Comms(roomba - test)/RF_Comms(roomba - test).ino
@@ -1,25 +1,44 @@
 #include "Motor_Driver.hpp"
 #include "Onboard_Comms.hpp"
+#include "RF_Comms.hpp"
 
 //Slave
 int motorPins[MOTOR_ROWS][MOTOR_COLS][NUM_MOTOR_PINS];
 Motor_Driver testDriver;
-
-
 CommsIn comms;
-
-
 void receivedData(int numBytes)
 {
   comms.commsRecieved(numBytes);
 }
 
-void setup()
-{
+//RF comms test code
+//Receiver: Receives data from the dock
+
+dataOut_t test_in;
+test_in.US_reading_1 = 55.0;
+test_in.US_reading_2 = 66.0;
+test_in.x_pos = 7.0;
+test_in.y_pos = 8.0;
+test_in.z_pos = 9.0;
+test_in.roomba_state = 2;
+test_in.roomba_tx_address = "00002";
+
+Rf_Comms test_comms_in;
+
+void setup() {
   Wire.begin(SLAVE_ADDR);
+
+  test_comms.initAddress();
+  test_comms.init();
+
+  test_comms.Bot_sendData(test_in);
+  Serial.println("Data sent from roomba");
+  delay(1000);
 }
 
 void loop()
 {
-    
+  test_comms.Bot_readData(test_out);
+  Serial.println("Data received from dock");
+  delay(1000);
 }
