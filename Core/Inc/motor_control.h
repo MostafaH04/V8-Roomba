fe43b07f50415c38c90d8164ec37459fef99f47d
MostafaH04 <mahussein04@gmail.com>
2024-02-10 14:25:33 -0500

    Initiial draft of motor control

---
+++ Core/Inc/motor_control.h
@@ -0,0 +1,70 @@
+/*
+ * motor_control.h
+ *
+ *  Created on: Feb 10, 2024
+ *      Author: mahus
+ */
+
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+#include "tim.h"
+
+#define AUTO_RELOAD_REG 839
+
+/* Motor */
+typedef struct {
+	TIM_HandleTypeDef* timer_fw;
+	uint32_t channel_fw;
+
+	TIM_HandleTypeDef* timer_bw;
+	uint32_t channel_bw;
+
+	float speed;
+	float wheel_radius;
+} Motor_t;
+
+Motor_t MOTOR_init(
+		TIM_HandleTypeDef* timer_fw, uint32_t channel_fw,
+		TIM_HandleTypeDef* timer_bw, uint32_t channel_bw,
+		float radius
+);
+
+void MOTOR_drive(Motor_t* motor);
+void MOTOR_set_speed(Motor_t* motor, float speed);
+
+
+/* Chassis Controller */
+
+typedef struct {
+	Motor_t *motors[4];
+	float wheel_base;
+} Chassis_Controller_t;
+
+
+Chassis_Controller_t CHASSIS_init(
+		Motor_t* left_front,
+		Motor_t* left_back,
+		Motor_t* right_front,
+		Motor_t* right_back,
+		float wheel_base
+);
+
+void CHASSIS_set_speed(
+		Chassis_Controller_t* chassis,
+		float speed,
+		float omega
+);
+
+void CHASSIS_drive(Chassis_Controller_t* chassis);
+
+static float calculate_wheel_rot(
+		float speed,
+		float omega,
+		float radius,
+		float wheel_base,
+		uint8_t right
+);
+
+
+#endif /* INC_MOTOR_CONTROL_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -87,8 +88,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  LIDAR_init(&huart1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -96,7 +99,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	LIDAR_start_scan(&g_lidar);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
